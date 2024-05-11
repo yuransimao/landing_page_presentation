2dff613324df6feae207b844ace87c7302b83126
yuransimao <yuransimao7@gmail.com>
2024-05-11 19:52:10 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+const MenuIcon =  document.querySelector("#menu-icon");
+const Navlist = document.querySelector(".navlist")
+
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", this.scrollY > 80)
+})
+MenuIcon.onclick = () => {
+    MenuIcon.classList.toggle("bx-x")
+    Navlist.classList.toggle("open")
+}
+
+
+window.onscroll = () => {
+    MenuIcon.classList.remove("bx-x")
+    Navlist.classList.remove("open")
+}
+
